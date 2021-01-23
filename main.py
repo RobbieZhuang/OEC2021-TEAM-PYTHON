6bce537f1e26942111970d6b4ef165d335212b4d
Bimesh De Silva <b2desilv@edu.uwaterloo.ca>
2021-01-23 12:30:36 -0500

    main

--- main.py
+++ main.py
@@ -10,4 +10,42 @@ def initialize_exposures():
         exposures[transition] = ExposureChance(transition, TRANSITION_EXPOSURE_FACTOR)
 
     for ec in ECS:
-        exposures[c] = ExposureChance(ec, EC_EXPOSURE_FACTOR)
+        exposures[ec] = ExposureChance(ec, EC_EXPOSURE_FACTOR)
+        transition = ec + ' Transition'
+        exposures[transition] = ExposureChance(transition, TRANSITION_EXPOSURE_FACTOR)
+
+    return exposures
+    
+def run_simluation(exposures, students):
+    def have_class():
+        for e in exposures:
+            
+
+    def ecs():
+        pass
+
+    def period_transition():
+        pass
+
+    def lunch():
+        pass
+
+    have_class()
+    period_transition()
+    have_class()
+    period_transition()
+    lunch()
+    have_class()
+    period_transition()
+    have_class()
+    period_transition()
+    ecs()
+
+    show_exposures()
+
+def show_exposures():
+    pass
+
+
+
+    
